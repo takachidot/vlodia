2546d23699b5da986fffce15f1211ee025ae8214
Yahya <109786377+takachidot@users.noreply.github.com>
2024-06-29 12:40:20 +0300

    Update types.ts

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,25 +1,7 @@
-import { Document, Schema } from "mongoose";
-
 export interface WriteOptions {
 	write?: boolean;
 	pretty?: boolean;
 }
-
-/*export interface MongoOptions {
-	useCreateIndex?: boolean;
-	useNewUrlParser?: boolean;
-	useUnifiedTopology?: boolean;
-	useFindAndModify?: boolean;
-}
-
-export interface MongoModel extends Document {
-	key: string;
-	value: Schema.Types.Mixed;
-}
-Still coding...
-*/  
-
-
 
 export interface ParsedObject {
 	key: string | undefined;
